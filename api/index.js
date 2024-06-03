955c9d2203d8afe1acd4cd781a45439319813b23
HassanKhan8264 <HF610061@GMAIL.COM>
2024-06-04 03:57:40 +0500

    update

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
-// const cors = require('cors');
+const cors = require('cors');
 const express = require('express');
 const config = require("./../backend/config/config");
 const router = require('./../backend/userInfo');
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
 
 app.use('/api', router);
-app.get("/", (req, res) => res.send("Express on Vercel"));
+
 
 
 app.listen(config.PORT, () => {
