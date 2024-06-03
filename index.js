531c8cfa6e1b9608e251af712d66f3f16b878622
HassanKhan8264 <HF610061@GMAIL.COM>
2024-06-04 03:30:30 +0500

    rm cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const cors = require('cors');
+// const cors = require('cors');
 const express = require('express');
 const config = require('./backend/config/config');
 const router = require('./backend/userInfo/index');
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 
 app.use('/api', router);
