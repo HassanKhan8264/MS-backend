27aaa7d2401dcec4d7a8009be455e8e016ac3833
HassanKhan8264 <HF610061@GMAIL.COM>
2024-06-04 02:52:55 +0500

    rm create server

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const cors = require('cors');
 const express = require('express');
-const http = require('http');
 const config = require('./backend/config/config');
 const router = require('./backend/userInfo/index');
 const bodyParser = require('body-parser');
 const app = express();
+
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cors());
 
-const server = http.createServer(app);
 
 app.use('/api', router);
 
 
-server.listen(config.PORT, () => {
+app.listen(config.PORT, () => {
   console.log(`Server is running on port ${config.PORT}`);
 });
