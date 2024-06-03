68840cd6f5a504243d1b5d7d65b668ca671e560f
HassanKhan8264 <HF610061@GMAIL.COM>
2024-06-04 03:19:10 +0500

    some fixex

--- backend/userInfo/index.js
+++ backend/userInfo/index.js
@@ -5,9 +5,9 @@ const express = require('express')
 const router = express.Router();
 
 
+router.get('/api/getAllUsers', UserCtrl.getAllUsers);
+router.post('/api/addUser', UserCtrl.addUser);
 
-router.get('/api/getAllUsers', UserCtrl.getAllUsers)
-router.post('/api/addUser', UserCtrl.addUser)
 
 module.exports = router
 
