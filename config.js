e10f462b14214310e7668dd3a552d5216351f3eb
HassanKhan8264 <HF610061@GMAIL.COM>
2024-06-04 21:20:57 +0500

    evn update

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
-dotenv.config();
+// Determine the environment based on NODE_ENV or a custom logic
+let envFile = '.env.local'; // Default to local environment
+if (process.env.NODE_ENV === 'production') {
+    envFile = '.env.production';
+}
+
+// Load the environment variables from the chosen.env file
+dotenv.config({ path: envFile });
 
 if (!process.env.MONGODB) {
     throw new Error("Missing MONGODB environment variable");
