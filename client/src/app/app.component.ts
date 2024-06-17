2ff69cb899b4083ef5a7811e70159ca165c42daf
HassanKhan8264 <HF610061@GMAIL.COM>
2024-06-18 01:01:03 +0500

    new build

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from "@angular/core";
-import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
-import { filter, map, mergeMap } from "rxjs/operators"; // Note the import statement for rxjs operators
-import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
   selector: "ss-root",
@@ -9,37 +6,4 @@ import { Meta, Title } from "@angular/platform-browser";
 })
 export class AppComponent {
   title = "mystudysolution-spa";
-
-  constructor(
-    private titleService: Title,
-    private metaService: Meta,
-    private router: Router,
-    private activatedRoute: ActivatedRoute
-  ) {}
-
-  ngOnInit(): void {
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof NavigationEnd),
-        map(() => this.activatedRoute),
-        map((route) => {
-          while (route.firstChild) route = route.firstChild;
-          return route;
-        }),
-        mergeMap((route) => route.data)
-      )
-      .subscribe((data) => {
-        const title = data["title"] || "Default Title";
-        const description = data["description"] || "Default Description";
-        this.updateMetaTags(title, description);
-      });
-  }
-
-  updateMetaTags(title: string, description: string) {
-    this.titleService.setTitle(title);
-    this.metaService.updateTag(
-      { name: "description", content: description },
-      `name='description'`
-    );
-  }
 }
