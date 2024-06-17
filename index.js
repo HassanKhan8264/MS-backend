14d2ae00a5b0ba4a8764a589c8c53dc9f92beab7
HassanKhan8264 <HF610061@GMAIL.COM>
2024-06-18 00:14:55 +0500

    build

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json())
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use('/api/v1', router);
+app.use('/', express.static(path.join(path.resolve(__dirname), './client/dist')))
+app.use('*', express.static(path.join(path.resolve(__dirname), './client/dist')))
 
 
 app.listen(config.PORT, () => {
