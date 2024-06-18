23cdbe438ad2449e3ec84cd3162845cb28f257eb
HassanKhan8264 <HF610061@GMAIL.COM>
2024-06-18 19:26:50 +0500

    change index

--- client/src/app/shared/services/seo.service.ts
+++ client/src/app/shared/services/seo.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@angular/core";
+import { Title, Meta } from "@angular/platform-browser";
 
 @Injectable({
   providedIn: "root",
 })
-export class SeoService {}
+export class SeoService {
+  constructor(private titleService: Title, private metaService: Meta) {}
+}
